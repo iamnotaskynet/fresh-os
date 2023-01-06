bdda73d1b5d6cf2e8be7c3aa23d90bfbc09bddd9
iamnotaskynet <iamnotaskynet@gmail.com>
2023-01-06 05:32:47 +0200

    vs code and empty winget

--- fedora.sh
+++ fedora.sh
@@ -53,4 +53,7 @@ sudo dnf install -y skypeforlinux
 sudo dnf install -y keepassxc
 
 # VSCode
-flatpak install https://flathub.org/repo/appstream/com.visualstudio.code.flatpakref
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install -y code
